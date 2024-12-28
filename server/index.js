3398745983264dcfa0fdd7e498f06f4466981f8b
Divyansh <divyanshraj0408@gmail.com>
2024-12-28 16:51:54 +0530

    academics routes intialized

--- server/index.js
+++ server/index.js
@@ -17,13 +17,24 @@ const aboutUsVisionAndMission = require("./routes/about/visionAndMission.js");
 const aboutUsVcMessage = require("./routes/about/vcMessage.js");
 const aboutUsAnnualReport = require("./routes/about/annualReport/year.js");
 const aboutUsGoverningBodies = require("./routes/about/governingBodies.js");
-const aboutUsBrochure = require("./routes/about/brochure.js");
+const aboutUsBrouchure = require("./routes/about/brouchure.js");
 const aboutUsColleges = require("./routes/about/aboutColleges.js");
 const aboutUsDisabledFriendliness = require("./routes/about/policies/disabledFriendliness.js");
 const aboutUsPoliciesGreenPolicy = require("./routes/about/policies/greenPolicy.js");
 const aboutUsPoliciesEgovernance = require("./routes/about/policies/egovernance.js");
 
+const academicsResearch = require("./routes/academics/research.js");
+const listOfColleges = require("./routes/academics/listOfColleges.js");
+const academicsFaculty = require("./routes/academics/faculty.js");
+const academicsSyllabus = require("./routes/academics/syllabus/revisedSyllabus.js");
+const UGSyllabus = require("./routes/academics/syllabus/UGSyllabus.js");
+const PGSyllabus = require("./routes/academics/syllabus/PGSyllabus.js");
+const DiplomaSyllabus = require("./routes/academics/syllabus/diplomaSyllabus.js");
+const CertificateSyllabus = require("./routes/academics/syllabus/certificateCourse.js");
+const academicCalender = require("./routes/academics/academicCalender.js");
+
 app.use("/api/", homeRoutes);
+//about us routes
 app.use("/api/aboutUs/memoir", aboutUsMemoir);
 app.use("/api/aboutUs/memoir/convocation", aboutUsMemoirConvocation);
 app.use("/api/aboutUs/memoir/formerVcs", aboutUsMemoirFormerVcs);
@@ -32,7 +43,7 @@ app.use("/api/aboutUs/vcMessage", aboutUsVcMessage);
 app.use("/api/aboutUs/visionAndMission", aboutUsVisionAndMission);
 app.use("/api/aboutUs/annualReport", aboutUsAnnualReport);
 app.use("/api/aboutUs/governingBodies", aboutUsGoverningBodies);
-app.use("/api/aboutUs/brochure", aboutUsBrochure);
+app.use("/api/aboutUs/brochure", aboutUsBrouchure);
 app.use("/api/aboutUs/colleges", aboutUsColleges);
 app.use(
   "/api/aboutUs/policies/disabledFriendliness",
@@ -40,6 +51,18 @@ app.use(
 );
 app.use("/api/aboutUs/policies/greenPolicy", aboutUsPoliciesGreenPolicy);
 app.use("/api/aboutUs/policies/egovernance", aboutUsPoliciesEgovernance);
+
+//academics routes
+app.use("/api/academics/research", academicsResearch);
+app.use("/api/academics/listofcolleges", listOfColleges);
+app.use("/api/academics/faculty", academicsFaculty);
+app.use("/api/academics/syllabus", academicsSyllabus);
+app.use("/api/academics/revisedSyllabus", academicsSyllabus);
+app.use("/api/academics/UGSyllabus", academicsSyllabus);
+app.use("/api/academics/PGSyllabus", academicsSyllabus);
+app.use("/api/academics/diplomaSyllabus", academicsSyllabus);
+app.use("/api/academics/certificateSyllabus", academicsSyllabus);
+app.use("/api/academics/academicCalender", academicCalender);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
--- server/routes/academics/syllabus/certificateCourse.js
+++ server/routes/academics/syllabus/certificateCourse.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("<h1>brouchure Route</h1>");
+  res.send("<h1>certificate Route</h1>");
 });
 
 module.exports = router;
