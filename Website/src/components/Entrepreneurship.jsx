9d201ff3522816dfabd3e7be6fdfe13965f5006b
Shreya <shreya186btcse23@igdtuw.ac.in>
2025-01-03 12:26:37 +0530

    entrepreneurship page done

--- Website/src/components/Entrepreneurship.jsx
+++ Website/src/components/Entrepreneurship.jsx
@@ -1,71 +1,148 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import EntrepreneurshipEvents from "./EntrepreneurshipEvents";
-// passion, ambition, vision, excellence, creativity, strategy
+import DSEU from "../assets/Image1.png";
+import DSEU1 from "../assets/Image2.png";
 
 const Entrepreneurship = () => {
+  const keywords = [
+    "passion",
+    "ambition",
+    "vision",
+    "excellence",
+    "creativity",
+    "strategy",
+  ];
+
+
+  const wrapperStyle = {
+    fontFamily: "Inter, sans-serif",
+    lineHeight: "1.6",
+    color: "black",
+    width: "100%",
+  };
+
+  const sectionStyle = {
+    textAlign: "center",
+    marginBottom: "40px",
+  };
+
+  const gridStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fit, minmax(320px, 1fr))",
+    gap: "20px",
+    width: "90%",
+    marginBottom: "20px",
+  };
+
+  const buttonStyle = {
+    padding: "13px 23px",
+    fontSize: "17px",
+    backgroundColor: "#ffb733",
+    color: "#003e70",
+    border: "none",
+    borderRadius: "7px",
+    cursor: "pointer",
+    fontWeight: "bold",
+  };
+
+  const spanStyle = {
+    color: "black",
+    animation: "spin_words 3s infinite",
+  };
+
+  const animatedSpan = {
+    display: "inline-block",
+  };
+
+  const styles = {
+    body: {
+      padding: "2em",
+      font: "normal 40px/50px Inter, sans-serif",
+      color: "black",
+    },
+    paragraph: {
+      height: "50px",
+      float: "left",
+      marginRight: "0.3em",
+      fontWeight: "700",
+      marginTop: "-1.5em",
+      marginLeft: '70px',
+      letterSpacing: "-3px",
+    },
+    bold: {
+      float: "left",
+      overflow: "hidden",
+      position: "absolute",
+      height: "50px",
+      marginLeft: '10px',
+      fontWeight: "700",
+    },
+    span: {
+      color: "black",
+      position: "relative",
+      whiteSpace: "nowrap",
+      top: "0",
+      left: "0",
+      animation: "move 5s infinite 1s",
+    },
+  };
+
   return (
     <>
-      <div
-        style={{
-          fontFamily: "Inter, sans-serif",
-          lineHeight: "1.6",
-          color: "black",
-          width: "100%",
-        }}
-      >
+      <div style={wrapperStyle}>
         <header style={{ textAlign: "left", marginBottom: "40px" }}>
-          <h1
-            style={{
-              fontSize: "45px",
-              fontWeight: "bold",
-              margin: "10px 20px",
-              padding: "20px",
-              letterSpacing: "-3px",
-              marginLeft: "8.5%",
-              textAlign: "left",
-            }}
-          >
-            Nurturing entrepreneurial minds to <br /> revolutionize industries{" "}
-            <br />
-            with passion
-          </h1>
+          <div style={styles.body}>
+            <p style={styles.paragraph}>
+              Nurturing entrepreneurial minds to revolutionize <br /> industries with{" "}
+              <b style={styles.bold}>
+                <span style={styles.span}>
+                  passion
+                  <br />
+                  ambition
+                  <br />
+                  vision
+                  <br />
+                  excellence
+                  <br />
+                  creativity
+                  <br />
+                  strategy
+                </span>
+              </b>
+            </p>
+          </div>
           <div
             style={{
               height: "500px",
               maxWidth: "80%",
-              backgroundColor: "#f0f0f0",
-              margin: "20px auto",
+              margin: "auto",
               borderRadius: "8px",
             }}
-          ></div>
+          >
+            <img
+              src={DSEU1}
+              alt=""
+              style={{
+                height: "100%",
+                width: "100%",
+                borderRadius: "4px",
+              }}
+            />
+          </div>
         </header>
 
         <main>
-          <section style={{ textAlign: "center", marginBottom: "40px" }}>
+          <section style={sectionStyle}>
             <h2
               style={{
                 fontSize: "2.2em",
-                fontWeight: "600",
-                margin: "20px 0",
+                fontWeight: "700",
+                margin: "10px 0",
+                // borderBottom: "2px solid black",
+                // display: "inline-block",
               }}
             >
-              <span
-                style={{
-                  borderBottom: "2px solid black",
-                  display: "inline-block",
-                }}
-              >
-                DSEU Innovation and Incubation Centre{" "}
-              </span>
-              <br />
-              <span
-                style={{
-                  borderBottom: "2px solid black",
-                  display: "inline-block",
-                }}
-              >
-                for Entrepreneurship (DIICE)
-              </span>
+              DSEU Innovation and Incubation Centre for Entrepreneurship (DIICE)
             </h2>
           </section>
 
@@ -73,26 +150,45 @@ const Entrepreneurship = () => {
             style={{
               maxWidth: "90%",
               margin: "0 auto",
+              marginLeft: "90px",
               textAlign: "justify",
+              marginTop: "-30px",
             }}
           >
-            <h3 style={{ fontSize: "1.5em", marginBottom: "10px" }}>
+            <h3
+              style={{
+                fontSize: "1.5em",
+                marginBottom: "5px",
+                borderBottom: "2px solid black",
+                marginTop: "-20px",
+                display: "inline-block",
+                paddingBottom: "-15px",
+              }}
+            >
               Introduction
             </h3>
-            <p>
+            <p
+              style={{
+                maxWidth: "97%",
+              }}
+            >
               The Delhi Skill and Entrepreneurship University (DSEU) was
               established in August 2020 by the Government of NCT of Delhi
               (GNCTD) to equip students with world-class skill education to
               enable access to aspirational jobs, inculcate an entrepreneurial
-              mindset and entrepreneurship. Entrepreneurship is an integral
-              part of DSEU's vision and mission. Coming up with a new business
-              idea is only a start; building a successful business around it is
-              the challenge. Solving this challenge needs an entrepreneurial
-              mindset, i.e., a person who is a voracious learner, has strong
-              communication and problem-solving skills, with a high motivation
-              to succeed, and yet open to risk and failure.
+              mindset and entrepreneurship. Entrepreneurship is an integral part
+              of DSEU’s vision and mission. Coming up with a new business idea
+              is only a start, building a successful business around it is the
+              challenge. Solving this challenge needs an entrepreneurial
+              mindset, i.e. a person who is creative, has strong communication
+              and problem-solving skills, with a high motivation to succeed, and
+              yet open to risk and failure.
             </p>
-            <p>
+            <p
+              style={{
+                maxWidth: "97%",
+              }}
+            >
               To provide a platform to encourage and support entrepreneurial
               endeavors, GNCTD has set up an incubator, DSEU Innovation and
               Incubation Centre for Entrepreneurship (DIICE). DIICE has been set
@@ -100,70 +196,56 @@ const Entrepreneurship = () => {
               Directors. To streamline all entrepreneurship and incubation
               activities at DSEU, a consolidation of all incubation programs
               under the various campuses of DSEU was proposed under one
-              incubator model premise, including programs like Foundation's
-              BUNI, promoted by Bhai Parmanand DSEU Shakarpur campus. Other such
-              programs are also in the process of being consolidated.
+              incubator and hence, BPIBS Knowledge and Innovation Foundation
+              (BKIF), promoted by Bhai Parmanand DSEU Shakarpur campus. Other
+              such programs are also in the process of being consolidated.
             </p>
           </section>
 
           <div
             style={{
-              fontFamily: "Arial, sans-serif",
+              fontFamily: "Inter, sans-serif",
               display: "flex",
               flexDirection: "column",
               alignItems: "center",
               padding: "20px",
             }}
           >
-            {/* Responsive Grid Section */}
-            <div
-              style={{
-                display: "grid",
-                gridTemplateColumns: "repeat(auto-fit, minmax(320px, 1fr))",
-                gap: "20px",
-                width: "90%",
-                marginBottom: "20px",
-              }}
-            >
+            <div style={gridStyle}>
               {[...Array(6)].map((_, index) => (
                 <div
                   key={index}
                   style={{
-                    backgroundColor: "#dcdcdc", // Gray background
-                    height: "200px", // Height of each card
-                    borderRadius: "8px", // Rounded corners
+                    backgroundColor: "#dcdcdc",
+                    height: "200px",
+                    borderRadius: "8px",
                   }}
-                ></div>
+                >
+                  <img
+                    src={DSEU}
+                    alt=""
+                    style={{
+                      height: "100%",
+                      width: "100%",
+                      borderRadius: "4px",
+                    }}
+                  />
+                </div>
               ))}
             </div>
 
-            <button
-              style={{
-                padding: "10px 20px",
-                fontSize: "16px",
-                backgroundColor: "#ffb733", // Orange color
-                color: "#003e70",
-                border: "none",
-                borderRadius: "4px",
-                cursor: "pointer",
-                fontWeight: "bold",
-              }}
-            >
-              Know More
-            </button>
+            <button className="button">Know More</button>
           </div>
         </main>
-      </div>
 
-      <main>
-        <section style={{ textAlign: "center", marginBottom: "40px" }}>
+        <section style={sectionStyle}>
           <h2
             style={{
               fontSize: "2.2em",
               fontWeight: "600",
               margin: "20px 0",
-              borderBottom: "2px solid black",
-              display: "inline-block",
+              // borderBottom: "2px solid black",
+              // display: "inline-block",
             }}
           >
             DSEU - SIDBI Entrepreneurship Programme
@@ -171,12 +253,31 @@ const Entrepreneurship = () => {
         </section>
 
         <section
-          style={{ maxWidth: "90%", margin: "0 auto", textAlign: "justify" }}
+          style={{
+            maxWidth: "90%",
+            margin: "0 auto",
+            marginLeft: "90px",
+            textAlign: "justify",
+            marginTop: "-30px",
+          }}
         >
-          <h3 style={{ fontSize: "1.5em", marginBottom: "10px" }}>
+          <h3
+            style={{
+              fontSize: "1.5em",
+              marginBottom: "5px",
+              borderBottom: "2px solid black",
+              marginTop: "-20px",
+              display: "inline-block",
+              paddingBottom: "-15px",
+            }}
+          >
             Introduction
           </h3>
-          <p>
+          <p
+            style={{
+              maxWidth: "97%",
+            }}
+          >
             The Delhi Skill and Entrepreneurship University (DSEU) was
             established in August 2020 by the Government of NCT of Delhi (GNCTD)
             to equip students with world-class skill education to enable access
@@ -189,7 +290,11 @@ const Entrepreneurship = () => {
             skills, with a high motivation to succeed, and yet open to risk and
             failure.
           </p>
-          <p>
+          <p
+            style={{
+              maxWidth: "97%",
+            }}
+          >
             To provide a platform to encourage and support entrepreneurial
             endeavors, GNCTD has set up an incubator, DSEU Innovation and
             Incubation Centre for Entrepreneurship (DIICE). DIICE has been set
@@ -201,56 +306,40 @@ const Entrepreneurship = () => {
             Bhai Parmanand DSEU Shakarpur campus. Other such programs are also
             in the process of being consolidated.
           </p>
+          <p
+            style={{
+              maxWidth: "97%",
+            }}
+          >
+            Many DSEU programs (e.g., Interior design, Fashion design,
+            Aesthetics & Beauty) are designed to promote freelancing/creation of
+            traditional businesses, as evidenced by a rich pool of alumni
+            entrepreneurs. Thus, the incubator is expected to support ventures
+            like these, in addition to typical product/process innovation
+            start-ups. Our platform is primarily available for the students,
+            alumni, faculty and staff of DSEU. However, we are open to be
+            engaged with people from outside the DSEU fraternity as well, who
+            have strong passion and ideas to be on their own, to set up new
+            ventures and to create new businesses. DSEU has also introduced a
+            compulsory curriculum to inculcate an entrepreneurial mindset
+            amongst the students, which will be carried forward by the students
+            to their respective undergraduate programs as they become
+            intrapreneurs or entrepreneurs. 
+          </p>
         </section>
+      </div>
+      <div
+        style={{
+          fontFamily: "Inter, sans-serif",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "20px",
+        }}
+      >
+        <button className="button">Know More</button>
+      </div>
 
-        <div
-          style={{
-            fontFamily: "Arial, sans-serif",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            padding: "20px",
-          }}
-        >
-          {/* Grid Section */}
-          <div
-            style={{
-              display: "grid",
-              gridTemplateColumns: "repeat(auto-fit, minmax(320px, 1fr))",
-              gap: "20px",
-              width: "90%",
-              marginBottom: "20px",
-            }}
-          >
-            {[...Array(6)].map((_, index) => (
-              <div
-                key={index}
-                style={{
-                  backgroundColor: "#dcdcdc",
-                  height: "200px",
-                  borderRadius: "8px",
-                }}
-              ></div>
-            ))}
-          </div>
-
-          {/* Button Section */}
-          <button
-            style={{
-              padding: "10px 20px",
-              fontSize: "16px",
-              backgroundColor: "#ffb733",
-              color: "#003e70",
-              border: "none",
-              borderRadius: "4px",
-              cursor: "pointer",
-              fontWeight: "bold",
-            }}
-          >
-            Know More
-          </button>
-        </div>
-      </main>
       <EntrepreneurshipEvents />
     </>
   );
--- Website/src/components/EntrepreneurshipEvents.jsx
+++ Website/src/components/EntrepreneurshipEvents.jsx
@@ -1,95 +1,111 @@
-import React from 'react';
+import React from "react";
+import DSEU from "../assets/Image1.png";
 
 const EntrepreneurshipEvents = () => {
   const events = [
     {
-      title: 'Event 1',
-      description: 'Identifying students with the right entrepreneurial acumen for rising up to solve societal challenges.',
+      title: "Event 1",
+      description:
+        "Identifying students with the right entrepreneurial acumen for rising up to solve societal challenges.",
     },
     {
-      title: 'Event 2',
-      description: 'Building capabilities and developing mindsets and skills amongst students so that they are well equipped to set up enterprises.',
+      title: "Event 2",
+      description:
+        "Building capabilities and developing mindsets and skills amongst students so that they are well equipped to set up enterprises.",
     },
     {
-      title: 'Event 3',
-      description: 'Challenging students to work on building solutions for real-world current market problems.',
+      title: "Event 3",
+      description:
+        "Challenging students to work on building solutions for real-world current market problems.",
     },
     {
-      title: 'Event 4',
-      description: 'Building capabilities and developing mindsets and skills amongst students so that they are well equipped to set up enterprises.',
+      title: "Event 4",
+      description:
+        "Building capabilities and developing mindsets and skills amongst students so that they are well equipped to set up enterprises.",
     },
   ];
 
   return (
     <div
       style={{
-        fontFamily: 'Arial, sans-serif',
-        lineHeight: '1.6',
-        padding: '20px',
-        textAlign: 'center',
+        fontFamily: "Arial, sans-serif",
+        lineHeight: "1.6",
+        padding: "20px",
+        textAlign: "center",
       }}
     >
-      <h1 style={{ fontSize: '2em', fontWeight: 'bold', marginBottom: '20px', display: 'inline-block', borderBottom: '2px solid black' }}>
+      <h1
+        style={{
+          fontSize: "2em",
+          fontWeight: "bold",
+          marginBottom: "40px",
+          // display: "inline-block",
+          // borderBottom: "2px solid black",
+        }}
+      >
         ENTREPRENEURSHIP EVENTS
       </h1>
       <div
         style={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(auto-fit, minmax(375px, 1fr))', 
-          gap: '20px',
-          justifyContent: 'center',
-          alignItems: 'stretch',
-          maxWidth: '1000px',
-          margin: '0 auto',
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(400px, 1fr))",
+          gap: "20px",
+          justifyContent: "center",
+          alignItems: "stretch",
+          maxWidth: "1000px",
+          margin: "0 auto",
         }}
       >
         {events.map((event, index) => (
           <div
             key={index}
             style={{
-              backgroundColor: '#eaf3fc',
-              padding: '20px',
-              borderRadius: '8px',
-              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-              display: 'flex',
-              alignItems: 'center',
-              gap: '20px',
-              minHeight: '120px',
-              color: '#003e70',
+              backgroundColor: "#d0ddeb",
+              padding: "20px",
+              borderRadius: "8px",
+              boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+              display: "flex",
+              alignItems: "center",
+              gap: "20px",
+              minHeight: "120px",
+              color: "#003e70",
             }}
           >
             <div
               style={{
-                height: '150px',
-                width: '350px',
-                backgroundColor: '#dcdcdc',
-                borderRadius: '4px',
+                height: "150px",
+                width: "350px",
+                backgroundColor: "#dcdcdc",
+                borderRadius: "4px",
               }}
-            ></div>
-            <div style={{ textAlign: 'left' }}>
-              <h3 style={{ fontSize: '1.5em', fontWeight: 'bold', margin: '0 0 10px 0' }}>
+            ><img src= {DSEU} alt="" 
+            style={{
+              height: "100%",
+              width: "100%",borderRadius: "4px" }}/></div>
+            <div style={{ textAlign: "left" }}>
+              <h3
+                style={{
+                  fontSize: "1.5em",
+                  fontWeight: "bold",
+                  margin: "0 0 10px 0",
+                }}
+              >
                 {event.title}
               </h3>
-              <p style={{ fontSize: '0.95em', margin: '0', fontWeight: 'bold' }}>{event.description}</p>
+              <p
+                style={{ fontSize: "0.95em", margin: "0", fontWeight: "bold" }}
+              >
+                {event.description}
+              </p>
             </div>
           </div>
         ))}
       </div>
       <button
-            style={{
-              padding: "10px 20px",
-              fontSize: "16px",
-              backgroundColor: "#ffb733", // Orange color
-              color: "#003e70",
-              border: "none",
-              borderRadius: "4px",
-              cursor: "pointer",
-              fontWeight: "bold",
-              marginTop: "20px",
-            }}
-          >
-            Know More
-        </button>
+        className="button entreButton"
+      >
+        Know More
+      </button>
     </div>
   );
 };
